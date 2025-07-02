a265451ae8d0642b25f0c3044cb92e9b7b9aaf46
kananda09 <kanandaaraujo09@gmail.com>
2025-07-01 23:45:48 -0300

    Portifólio

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-let userHasScrolled = false;
-
-window.addEventListener("scroll", () => {
-  userHasScrolled = true;
-});
-
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      if (userHasScrolled && entry.isIntersecting) {
+      if (entry.isIntersecting) {
         entry.target.classList.add("animate");
-      } else if (!entry.isIntersecting) {
-        entry.target.classList.remove("animate"); // Remove ao sair da tela
+      } else {
+        entry.target.classList.remove("animate"); // Remove ao sair da tela, se quiser
       }
     });
   },
